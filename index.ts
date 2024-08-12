0f003931b5ee3a3fd7edf8e634e41c9c70d3cc43
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-08-12 13:28:32 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import {differenceInSeconds} from "date-fns"
+
+const  response  = await inquirer.prompt([
+    {
+     name : "Input",
+     type: "number",
+     message: "please enter the amount of second",
+     validate: (input)=> {
+        if (isNaN(input)){
+            return "Please enter valid number"
+        } else if (input> 60 ){
+            return "second must be in 60"
+        }else {
+            return true;
+        }
+     }
+    }
+]);
+
+
+let input =response.Input
+
+function  startTime(val:number){
+    const inTime = new Date(). setSeconds(new Date(). getSeconds() + val);    setInterval((()=> {}), 1000)
+     const intervalTime = new Date (inTime)
+     setInterval((() => {
+        const currentTime = new Date ()
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+
+        if(timeDiff <= 0){
+            console.log("TImer has expired");
+            process.exit()
+        }
+
+        const min = Math.floor ((timeDiff % (3600 *24))/3600)
+        const  sec =Math.floor (timeDiff%60)
+        console.log(`${min.toString().padStart(2,"0")} : ${sec.toString().padStart(2,"0")}`);
+     }),1000)
+}
+startTime(input)
